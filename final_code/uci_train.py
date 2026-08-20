7a4e326686bf3cccd9207f24b5a80792e773bc39
ankita2002 <ankitaupadhyaya209@gmail.com>
2026-08-20 13:38:15 +0200

    code. clean up and bug fixes

--- final_code/uci_train.py
+++ final_code/uci_train.py
@@ -140,8 +140,8 @@ def train(args):
 
     dataset = WindowDataset(
         series=train_series,
-        backcast_len=backcast_len,
-        forecast_len=forecast_len,
+        backcast_length=backcast_len,
+        forecast_length=forecast_len,
         stride=STRIDE,
     )
 
@@ -156,7 +156,7 @@ def train(args):
     )
 
 
-    model = NBeats(backcast_len=backcast_len, forecast_len=forecast_len, hidden_size=HIDDEN_SIZE, theta_size=THETA_SIZE, n_blocks=N_BLOCKS, n_layers=N_LAYERS,).to(device)
+    model = NBeats(backcast_length=backcast_len, forecast_length=forecast_len, hidden_size=HIDDEN_SIZE, theta_size=THETA_SIZE, n_blocks=N_BLOCKS, n_layers=N_LAYERS,).to(device)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE,  weight_decay=WEIGHT_DECAY, )
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau( optimizer, mode="min", factor=0.5, patience=3,)
